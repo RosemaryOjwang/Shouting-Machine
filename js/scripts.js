b1597d593c2f50776e58c348f2098d7ad8c1f483
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-10-31 15:05:28 +0300

    add increasingFontSize(), work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,18 @@ function getAndSetValues() {
   document.querySelector("span#whatYouSaid").innerText = formInput;
 }
 //This function will increase the fontsize of the out with every submission
+function increasingFontSize() {
+   getAndSetValues();
+   let fontSize = formInput.style.fontSize = "1.8em"
+   let increamentFontSize = 0.2;
+  }
 
 window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event)  {
     event.preventDefault();
     getAndSetValues();
+    //increasingFontSize();
 
     document.querySelector("div#yourOutput").removeAttribute("class");
   };
